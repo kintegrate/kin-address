3ed42f3784e206ad10a91ac9051b5835861caab8
Bram Borggreve <borggreve@gmail.com>
2021-03-23 23:54:44 -0500

    feat: allow continuous search

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Keypair } from './lib/kin-base/keypair'
 import { Command, flags } from '@oclif/command'
+import { gray, green } from 'kleur'
 
 class KinAddress extends Command {
   static description = 'Generate a key pair for Kin'
@@ -8,7 +9,7 @@ class KinAddress extends Command {
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
     attempts: flags.integer({ char: 'a', description: 'Number of attempts', default: 1000000 }),
-    force: flags.boolean({ char: 'f' }),
+    continue: flags.boolean({ char: 'c', description: 'Continue searching after first match', default: false }),
   }
 
   static args = [{ name: 'terms' }]
@@ -16,29 +17,32 @@ class KinAddress extends Command {
   async run() {
     const { args, flags } = this.parse(KinAddress)
 
-    if (args.terms) {
-      const terms: string[] = args.terms.includes(',') ? args.terms.split(',') : [args.terms]
-      const attempts: number = flags.attempts
+    if (!args.terms) {
+      const keys = Keypair.randomKeys()
+      this.log(`Secret    : ${keys.secret}`)
+      this.log(`Public Key: ${keys.publicKey}`)
+      return
+    }
 
-      for (let attempt = 0; attempt < attempts; attempt++) {
-        if (attempt % 1000 === 0) {
-          this.log(`Attempts ${attempt} for terms: ${terms.join(', ')}`)
-        }
-        const keys = Keypair.randomKeys()
-        const publicKey = keys.publicKey.toLowerCase()
-        for (const term of terms) {
-          if (publicKey.startsWith(term.toLowerCase())) {
-            this.log(`Found a match for ${term} (attempt ${attempt})!`)
-            this.log(`Secret    : ${keys.secret}`)
-            this.log(`Public Key: ${keys.publicKey}`)
+    const terms: string[] = args.terms.includes(',') ? args.terms.split(',') : [args.terms]
+    const attempts: number = flags.attempts
+
+    for (let attempt = 0; attempt < attempts; attempt++) {
+      if (attempt % 1000 === 0) {
+        this.log(gray(`Attempts ${attempt}/${attempts} for terms: ${terms.join(', ')}`))
+      }
+      const keys = Keypair.randomKeys()
+      const publicKey = keys.publicKey.toLowerCase()
+      for (const term of terms) {
+        if (publicKey.startsWith(term.toLowerCase())) {
+          this.log(`Found a match for ${green(term)} (attempt ${attempt})!`)
+          this.log(`Secret    : ${keys.secret}`)
+          this.log(`Public Key: ${green(keys.publicKey)}`)
+          if (!flags.continue) {
             return true
           }
         }
       }
-    } else {
-      const keys = Keypair.randomKeys()
-      this.log(`Secret    : ${keys.secret}`)
-      this.log(`Public Key: ${keys.publicKey}`)
     }
   }
 }
